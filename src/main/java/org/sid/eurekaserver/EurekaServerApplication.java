bf3e86c393639adb1e1f46d719db57db9570b0af
yassinerouis <56519992+yassinerouis@users.noreply.github.com>
2019-12-25 00:05:21 +0100

    second

--- src/main/java/org/sid/eurekaserver/EurekaServerApplication.java
+++ src/main/java/org/sid/eurekaserver/EurekaServerApplication.java
@@ -3,13 +3,11 @@ package org.sid.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
-
 	}
 
 }
